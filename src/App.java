48367c394223c3bd3e0e015b5a91fa9ec0f845cb
Ethan <ethanrosengozani@gmail.com>
2025-10-23 15:54:51 -0400

    october 23

--- src/App.java
+++ src/App.java
@@ -33,7 +33,7 @@ public class App extends PApplet {
         greenV = 0;
         startMs = millis();
         blueStarted = false;
-        int MaxSpeed = 5;
+
     }
 
     public void draw() {
@@ -44,7 +44,7 @@ public class App extends PApplet {
         rect(265, 0, 200, 600);
 
         fill(0);
-        rect(355, 0, 20, 50);
+        // rect(355, 0, 20, 50);
         rect(355, 80, 20, 50);
         rect(355, 160, 20, 50);
         rect(355, 240, 20, 50);
@@ -52,28 +52,63 @@ public class App extends PApplet {
         rect(355, 400, 20, 50);
         rect(355, 480, 20, 50);
         rect(355, 560, 20, 50);
-
-        textAlign(CENTER, CENTER);
-        textSize(24);
+        fill(255, 255, 0);
+        rect(200, 0, 40, 800);
+        rect(490, 0, 40, 800);
+        // checkers flag
         fill(0);
+        square(265, 0, 30);
+        fill(255);
+        square(295, 0, 30);
+        fill(0);
+        square(325, 0, 30);
+        fill(255);
+        square(355, 0, 30);
+        fill(0);
+        square(385, 0, 30);
+        fill(255);
+        square(415, 0, 30);
+        fill(255);
+        square(265, 30, 30);
+        fill(0);
+        square(295, 30, 30);
+        fill(255);
+        square(325, 30, 30);
+        fill(0);
+        square(355, 30, 30);
+        fill(255);
+        square(385, 30, 30);
+        fill(0);
+        square(415, 30, 30);
+        fill(255);
+        square(445, 30, 30);
+        fill(0);
+        square(445, 0, 30);
+
         int time = millis() - startMs;
         if (time < 1000) {
-            text("4 Seconds Left", width / 2f, height / 2f);
+            textSize(20);
+            text("4 Seconds Left", 50, 100);
         } else if (time < 2000) {
-            text("3 Seconds Left", width / 2f, height / 2f);
+            textSize(20);
+            text("3 Seconds Left", 50, 100);
         } else if (time < 3000) {
-            text("2 Seconds Left", width / 2f, height / 2f);
+            textSize(20);
+            text("2 Seconds Left", 50, 100);
         } else if (time < 4000) {
-            text("1 Second Left", width / 2f, height / 2f);
+            textSize(20);
+            text("1 Second Left", 50, 100);
         } else if (time < 5000) {
-            text("GO!", width / 2f, height / 2f);
+            fill(0);
+            textSize(20);
+            text("GO!", 100, 100);
         }
 
         if (millis() - startMs > 5000) {
             blueStarted = true;
         }
-        if (blueStarted && greenV < 5) {
-            greenV -= 0.1;
+        if (blueStarted == true) {
+            greenV -= 0.5;
 
             if (wDown) {
                 redV -= 0.1;
@@ -87,8 +122,8 @@ public class App extends PApplet {
             redY += redV;
             greenY += greenV;
 
-            if (blueStarted && greenV < 5) {
-                greenV -= 0.1;
+            if (greenV < 0) {
+                greenV = 0;
             }
             if (wDown) {
                 redV -= 0.1;
@@ -109,49 +144,91 @@ public class App extends PApplet {
 
             int S = 50;
 
-            if (redY + S < 0 && Redresets < 6) {
+            if (redY + S < 0 && Redresets < 3) {
                 redY = height;
                 Redresets++;
-            } else if (redY > height && Redresets < 6) {
+            } else if (redY > height && Redresets < 3) {
                 redY = -S;
                 Redresets++;
             }
 
-            if (greenY + S < 0 && Greenresets < 6) {
+            if (greenY + S < 0 && Greenresets < 3) {
                 greenY = height;
                 Greenresets++;
-            } else if (greenY > height && Greenresets < 6) {
+            } else if (greenY > height && Greenresets < 3) {
                 greenY = -S;
                 Greenresets++;
             }
 
-            if (Redresets >= 6 && Greenresets >= 6) {
+            if (Redresets >= 3 || Greenresets >= 3) {
                 noLoop();
                 fill(0);
-                textSize(80);
-                text("Game Over", width / 2f, height / 2f);
+                textSize(40);
+                text("Game Over", 600, 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+                fill(random(255), random(255), random(255));
+                square(random(600), random(400), 50);
+
+                if (Redresets > Greenresets) {
+                    fill(0);
+                    textSize(40);
+                    text("Red Wins!", 600, 150);
+                } else if (Greenresets > Redresets) {
+                    fill(0);
+                    textSize(40);
+                    text("Green Wins!", 600, 150);
+                } else {
+                    fill(0);
+                    textSize(40);
+                    text("It's a Tie!", 600, 250);
+                }
             }
-            if (Redresets > Greenresets) {
-                fill(0);
-                textSize(40);
-                text("Red Wins!", width / 2f, height / 2f + 100);
-            } else if (Greenresets > Redresets) {
-                fill(0);
-                textSize(40);
-                text("Green Wins!", width / 2f, height / 2f + 100);
-            } else {
-                fill(0);
-                textSize(40);
-                text("It's a Tie!", width / 2f, height / 2f + 100);
-            }
+
+            fill(255, 80, 0);
+            square(redX, redY, 50);
+
+            fill(0, 0, 255);
+            square(greenX, greenY, 50);
         }
-
-        // --- draw cars LAST so visuals match updated positions ---
-        fill(255, 80, 0);
-        square(redX, redY, 50);
-
-        fill(0, 0, 255);
-        square(greenX, greenY, 50);
     }
 
     public void keyPressed() {
